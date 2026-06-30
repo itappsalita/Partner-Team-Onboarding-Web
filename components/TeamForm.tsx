fa92d4debf07e655ff8355e181630d07a09778c1
amstrongnpsb <amstrongpsb@gmail.com>
2026-06-30 10:59:17 +0700

    feat: update alert when success edit team

--- components/TeamForm.tsx
+++ components/TeamForm.tsx
@@ -57,6 +57,7 @@ export default function TeamForm({
       });
 
       if (res.ok) {
+        alert(isEditMode ? "✅ Data tim lapangan berhasil diperbarui!" : "✅ Tim lapangan baru berhasil dibuat!");
         onSave();
         onClose();
       } else {
